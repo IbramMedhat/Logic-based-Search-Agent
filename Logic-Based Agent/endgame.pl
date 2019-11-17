84523a61c148a0783552c1f8ce346e21e85f7420
unknown <ibram.medhat2@gmail.com>
2019-11-17 20:57:16 +0200

    Adding Successor state axiom positionIM

--- Logic-Based Agent/endgame.pl
+++ Logic-Based Agent/endgame.pl
@@ -5,3 +5,40 @@ positionS(1, 1, s1).
 positionS(2, 1, s2).
 positionS(2, 2, s3).
 positionS(3, 3, s4).
+
+%Could be added that IM can not enter the cell of thanos
+%Successor state axiom for iron man position.
+positionIM(X, Y, result(A, S)) :-
+    %effect axioms
+    gridSize(Height, Width),
+    %effect of up on the previous position
+    (positionIM(X1, Y1, S),
+    X1 is X - 1,
+    Y1 is Y,
+    A = up,
+    X1 >= 0);
+    %effect of up on the previous position
+    (positionIM(X1, Y1, S),
+    X1 is X,
+    Y1 is Y - 1,
+    A = left,
+    Y1 >= 0);
+    %effect of up on the previous position
+    (positionIM(X1, Y1, S),
+    X1 is X + 1,
+    Y1 is Y,
+    A = down,
+    X1 < Height);
+    %effect of up on the previous position
+    (positionIM(X1, Y1, S),
+    X1 is X,
+    Y1 is Y + 1,
+    A = right,
+    Y1 < Width);
+    %Persistance axioms when action is not movement and the previous position is the same as the current position   
+    (positionIM(X, Y, S),
+    A \= left,
+    A \= right,
+    A \= up,
+    A \= down).
+    
