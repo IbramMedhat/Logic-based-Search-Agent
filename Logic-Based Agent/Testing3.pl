73fed6cd618d848ba1d9ae38d750e3551201b6fb
Steven Henry <stevennassef97@gmail.com>
2019-11-21 01:38:21 +0200

    NEW stone_collected

--- Logic-Based Agent/Testing3.pl
+++ Logic-Based Agent/Testing3.pl
@@ -1,13 +1,14 @@
 :- use_module(library(clpfd)).
 
-grid(0,4).
-posS(0,1,stone_1).
-posS(0,2,stone_2).
-% posS(0,3,stone_3).
-% posS(0,4,stone_4).
-posTH(0,3).
 
-posIM(0,0,s0).
+grid(5,5).
+posS(1,1,stone_1).
+posS(2,1,stone_2).
+% posS(2,2,stone_3).
+% posS(3,3,stone_4).
+posTH(3,4).
+
+posIM(1,2,s0).
 
 
 posIM(X_new,Y_new ,result(A,S)):-
@@ -25,7 +26,7 @@ posIM(X_new,Y_new ,result(A,S)):-
             (A = right, X_new #= X_old , Y_new #= Y_old + 1);
             (A = down, X_new #= X_old + 1 , Y_new #= Y_old);
             (A = up, X_new #= X_old - 1 , Y_new #= Y_old);
-            (A = collect, X_old #= X_new , Y_old #= Y_new, posS(X_old, Y_old ,Stone), not(stone_collected(Stone, S)))
+            (A = collect, X_old #= X_new , Y_old #= Y_new, posS(X_old, Y_old ,Stone), not(once(stone_collected(Stone,X_old,Y_old, S))))
         )
     )
 
@@ -50,38 +51,64 @@ posIM(X_new,Y_new ,result(A,S)):-
 %     pos(X_new,Y_new, iron_man, S),
 %     A = collect.
 
-stone_collected(_, s0):-
-    false.
-stone_collected(Stone, result(A, S)):-
+
+% stone_collected(Stone,X,Y, result(A, S)):-
     
-    %Effect
-    (
-        % implies(
-            Stone \= iron_man,
-            posS(X,Y, Stone), posIM(X,Y,S),
-            not(stone_collected(Stone, S)),
-            %(stone_collected(OtherStone, S), Stone \= OtherStone),
-            A = collect,
-            print(collect),print(Stone), nl
-        % )
-    );
-    %Persistance
-    (
+%     %Effect
+%     (
+%         % implies(
+%             Stone \= iron_man,
+%             posS(X,Y, Stone),
+%             %(stone_collected(OtherStone, S), Stone \= OtherStone),
+%             A = collect,
+%             not(stone_collected(Stone, X, Y,S))
+%             % print(collect),print(Stone), nl
+%         % )
+%     );
+%     %Persistance
+%     (
         
-        Stone \= iron_man, ( (  posS(X,Y, Stone), posIM(X,Y, S) , A \= collect, (A = left; A = right;A = down;A = up) ) ;
-        ( posS(X,Y, Stone), posIM(X1,Y1, S),(X1 \= X;Y1 \= Y),(A = left; A = right;A = down;A = up; A = collect)) )
-        ,stone_collected(Stone, S)
+%         Stone \= iron_man, posS(X1,Y1, Stone), ( (  X1 #= X , Y1 #= Y , A \= collect, (A = left; A = right;A = down;A = up) ) ;
+%         ( (X1 #\= X;Y1 #\= Y),(A = left; A = right;A = down;A = up; A = collect)) )
+%         ,stone_collected(Stone,X,Y, S)
+
         
-        ,print(persistance)
-    ).
+%         % ,print(persistance)
+%     ).
+
+stone_collected(_,_,_,s0):-
+    false.
+stone_collected(Stone, X, Y, result(collect, _)):-
+    posS(X,Y,Stone).
+stone_collected(Stone, X , Y , result(A, S)):-
+    % print(X),print(Y),print(S),print(A),nl,
+    (A = left , Y_old #= Y + 1 , stone_collected(Stone, X , Y_old, S) );
+    (A = right , Y_old #= Y - 1 , stone_collected(Stone, X , Y_old, S) );
+    (A = down , X_old #= X - 1 , stone_collected(Stone, X_old , Y, S) );
+    (A = up , X_old #= X + 1 , stone_collected(Stone, X_old , Y, S) ) .
+    
+    % stone_collected(Stone, X, Y, S).
+% stone_collected(Stone, X , Y , result(A, S)):-
+%     print(S),print(X),print(Y),nl,
+%     (posS(Stone,X1,Y1) , X1 #\= X, Y1 #\= Y ) ,
+%     (
+%         (A = collect , stone_collected(Stone, X, Y, S) );
+%         (A = left , Y_old #= Y + 1 , stone_collected(Stone, X , Y_old, S) );
+%         (A = right , Y_old #= Y - 1 , stone_collected(Stone, X , Y_old, S) );
+%         (A = down , X_old #= X - 1 , stone_collected(Stone, X_old , Y, S) );
+%         (A = up , X_old #= X + 1 , stone_collected(Stone, X_old , Y, S) ) 
+%         )
+%     ; ((posS(Stone, X1, Y1) , X1 = X, Y1 = Y) , A = collect).
+        
 
 snapped(S):-
     S = result(snap,S1),
     posIM(X,Y,S1),
     posTH(X,Y),
-    stone_collected(stone_2, S1),
-    stone_collected(stone_1, S1).
-    % stone_collected(stone_3, S1),
+    % print(S),nl,
+    stone_collected(stone_1, X, Y,S1),
+    stone_collected(stone_2, X, Y,S1).
+    % stone_collected(stone_3, S1).
     % stone_collected(stone_4, S1),
     %posIM(0,2,S).
 
