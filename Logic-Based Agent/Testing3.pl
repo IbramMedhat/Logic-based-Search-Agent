f5b19acc5b19697e5ef49f8366d3bb3aa262a96f
Steven Henry <stevennassef97@gmail.com>
2019-11-21 02:00:21 +0200

    Fixes in Stone_collected

--- Logic-Based Agent/Testing3.pl
+++ Logic-Based Agent/Testing3.pl
@@ -4,8 +4,8 @@
 grid(5,5).
 posS(1,1,stone_1).
 posS(2,1,stone_2).
-% posS(2,2,stone_3).
-% posS(3,3,stone_4).
+posS(2,2,stone_3).
+posS(3,3,stone_4).
 posTH(3,4).
 
 posIM(1,2,s0).
@@ -78,14 +78,17 @@ posIM(X_new,Y_new ,result(A,S)):-
 
 stone_collected(_,_,_,s0):-
     false.
-stone_collected(Stone, X, Y, result(collect, _)):-
-    posS(X,Y,Stone).
+% stone_collected(Stone, X, Y, result(collect, _)):-
+%     posS(X,Y,Stone).
 stone_collected(Stone, X , Y , result(A, S)):-
-    % print(X),print(Y),print(S),print(A),nl,
+    print(X),print(Y),print(S),print(A),nl,
     (A = left , Y_old #= Y + 1 , stone_collected(Stone, X , Y_old, S) );
     (A = right , Y_old #= Y - 1 , stone_collected(Stone, X , Y_old, S) );
     (A = down , X_old #= X - 1 , stone_collected(Stone, X_old , Y, S) );
-    (A = up , X_old #= X + 1 , stone_collected(Stone, X_old , Y, S) ) .
+    (A = up , X_old #= X + 1 , stone_collected(Stone, X_old , Y, S) );
+    (A = snap, stone_collected(Stone, X, Y, S));
+    (A = collect, posS(X, Y, Stone));
+    (A = collect, not(posS(X, Y, Stone)) , stone_collected(Stone, X, Y, S)).
     
     % stone_collected(Stone, X, Y, S).
 % stone_collected(Stone, X , Y , result(A, S)):-
@@ -107,9 +110,9 @@ snapped(S):-
     posTH(X,Y),
     % print(S),nl,
     stone_collected(stone_1, X, Y,S1),
-    stone_collected(stone_2, X, Y,S1).
-    % stone_collected(stone_3, S1).
-    % stone_collected(stone_4, S1),
+    stone_collected(stone_2, X, Y,S1),
+    stone_collected(stone_3, X, Y,S1),
+    stone_collected(stone_4, X, Y,S1).
     %posIM(0,2,S).
 
 snapped_with_limit(S, Limit) :-
@@ -124,6 +127,3 @@ implies(X, Y):-
     not(X) ; Y.
     
 
-pos(2).
-collected(2).
-collected(1).
