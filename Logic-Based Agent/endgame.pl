8ef17f92b1fd63550aa3dff889ba8c20ca8c62e6
unknown <ibram.medhat2@gmail.com>
2019-11-22 19:02:41 +0200

    including the KB using include predicate

--- Logic-Based Agent/endgame.pl
+++ Logic-Based Agent/endgame.pl
@@ -1,13 +1,7 @@
 :- use_module(library(clpfd)).
 
 
-grid(5, 5).
-posS(2, 3, stone_1).
-posS(3, 4, stone_2).
-posS(4, 4, stone_3).
-posS(4, 5, stone_4).
-posTH(5, 5).
-posIM(2, 2, s0).
+:- include('KB1.pl').
 
 
 posIM(X_new,Y_new ,result(A,S)):-
